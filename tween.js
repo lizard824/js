33799a88ac7ee79defa86053190e8095be6a6328
Kai Kai2 Shen <shenkai2@Lenovo.com>
2017-07-18 18:54:25 +0800

    123

---
+++ tween.js
@@ -0,0 +1,13 @@
+/**
+ * Created by shenkai2 on 7/14/2017.
+ */
+~function () {
+    var zhufengEffect={
+        Liner:function(t,b,c,d){
+            return c*t/d+b;
+        }
+    };
+    function move(curEle,target) {
+
+    }
+}();
